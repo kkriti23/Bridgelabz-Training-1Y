42ab4c56370911f4807356c4dc49682210495563
kkriti23 <kkriti.agl@gmai.,com>
2026-01-14 22:04:48 +0530

    Change distance

---
+++ BasicPrograms/ConvertDistance.java
@@ -0,0 +1,7 @@
+public class ConvertDistance{
+public static void main(String[] args){
+	double km = 10.8;
+	double miles = 10.8 * 1.6;
+	System.out.println("The distance "+km+"km in miles is "+miles);
+}
+}
