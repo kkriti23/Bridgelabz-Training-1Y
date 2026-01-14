7fa547f2dc6bf10f7197b98b9c0ea4afe8e59070
kkriti23 <kkriti.agl@gmai.,com>
2026-01-14 23:00:44 +0530

    couse fee user input

---
+++ BasicPrograms/CourseFeeUserInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class CourseFeeUserInput{
+public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	float fee = sc.nextFloat();
+//	float fee = 125000;
+	float discountPercent = 10;
+	float discount = fee*(discountPercent/100);
+	float final_fee = fee - discount;
+	System.out.println("The discount amount is INR "+discount+" and final discounted fee is INR "+final_fee);
+}
+}
