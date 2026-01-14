78b761142a3f6375e83fe5f24014eca675bcfeb6
kkriti23 <kkriti.agl@gmai.,com>
2026-01-14 20:59:19 +0530

    add two number

---
+++ BasicPrograms/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AddTwoNumbers{
+public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+int x = scanner.nextInt();
+int y = scanner.nextInt();
+int sum = x + y;
+System.out.println("Sum : "+ sum);
+}
+}
