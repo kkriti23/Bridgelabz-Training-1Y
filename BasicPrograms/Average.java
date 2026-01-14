77f49a1718f06901e8e0eb5366d41962f1068751
kkriti23 <kkriti.agl@gmai.,com>
2026-01-14 21:51:07 +0530

    Average of sam's marks

---
+++ BasicPrograms/Average.java
@@ -0,0 +1,9 @@
+public class Average{
+public static void main(String[] args){
+	int math = 94;
+	int phy = 95;
+	int chem = 96;
+	int avg = (math+phy+chem)/3;
+	System.out.println("Sam’s average mark in PCM is "+avg);
+}
+}
