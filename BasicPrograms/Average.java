a13cc412c10539a7d1df9afe9d34139caa47172d
kkriti23 <kkriti.agl@gmai.,com>
2026-01-22 13:15:03 +0530

    Arguments

--- BasicPrograms/Average.java
+++
@@ -1,9 +0,0 @@
-public class Average{
-public static void main(String[] args){
-	int math = 94;
-	int phy = 95;
-	int chem = 96;
-	int avg = (math+phy+chem)/3;
-	System.out.println("Sam’s average mark in PCM is "+avg);
-}
-}
--- BasicPrograms/ConvertDistance.java
+++
@@ -1,7 +0,0 @@
-public class ConvertDistance{
-public static void main(String[] args){
-	double km = 10.8;
-	double miles = 10.8 * 1.6;
-	System.out.println("The distance "+km+"km in miles is "+miles);
-}
-}
