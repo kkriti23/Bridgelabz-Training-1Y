27ccaf0632a9249c6f43377e2b7c9b287b4faddd
kkriti23 <kkriti.agl@gmai.,com>
2026-01-24 15:27:18 +0530

    KRITI (add) factorial using while loop

---
+++ BasicPrograms/WhileFactorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class WhileFactorial{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        
+        if(n>0){
+            int factorial = 1;
+            for(int i=n;i>0;i--){
+                factorial*=i;
+            }
+            System.out.println("factorial : "+factorial);
+        }else{
+            System.out.println("Not natural number");
+        }
+        
+    }
+}
