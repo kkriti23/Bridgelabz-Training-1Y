f5779f02e60757cacee3a2696474fc125dd9c0d8
kkriti23 <kkriti.agl@gmai.,com>
2026-01-24 20:03:40 +0530

    KRITI (add)  factors using for loop

---
+++ BasicPrograms/FactorViaFor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FactorViaFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number :");
+        int n = sc.nextInt();
+        System.out.println("Factors are :");
+        for(int i=1 ; i<=n ; i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
